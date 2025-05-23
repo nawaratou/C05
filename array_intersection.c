c16ba72fd7f95601b4de8b742375114141f5e20d
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 00:56:53 +0100

    corrige

--- array_intersection.c
+++ array_intersection.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 
+int est_deja_affiche(int arr[], int taille, int valeur) {
+    // Vérifie si 'valeur' est déjà dans 'arr'
+    for (int i = 0; i < taille; i++) {
+        if (arr[i] == valeur) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main() {
     int n1, n2;
-    scanf("%d", &n1);  // Taille du 1er tableau
+    scanf("%d", &n1);
     int tab1[n1];
     for (int i = 0; i < n1; i++) {
         scanf("%d", &tab1[i]);
     }
 
-    scanf("%d", &n2);  // Taille du 2e tableau
+    scanf("%d", &n2);
     int tab2[n2];
     for (int i = 0; i < n2; i++) {
         scanf("%d", &tab2[i]);
     }
 
-    printf("Intersection : ");
+    int resultat[n1 < n2 ? n1 : n2]; // Taille maximale possible de l'intersection
+    int taille_resultat = 0;
+
     for (int i = 0; i < n1; i++) {
-        int commun = 0;
-
-        // Vérifier si l'élément tab1[i] est dans tab2
         for (int j = 0; j < n2; j++) {
-            if (tab1[i] == tab2[j]) {
-                commun = 1;
-                break;
+            if (tab1[i] == tab2[j] && !est_deja_affiche(resultat, taille_resultat, tab1[i])) {
+                // Si élément commun et pas déjà ajouté
+                resultat[taille_resultat] = tab1[i];
+                taille_resultat++;
             }
         }
+    }
 
-        // Vérifier qu’il n’est pas déjà affiché
-        int deja_affiche = 0;
-        for (int k = 0; k < i; k++) {
-            if (tab1[i] == tab1[k]) {
-                deja_affiche = 1;
-                break;
-            }
-        }
-
-        if (commun && !deja_affiche) {
-            printf("%d ", tab1[i]);
-        }
+    printf("Intersection : ");
+    for (int i = 0; i < taille_resultat; i++) {
+        printf("%d ", resultat[i]);
     }
     printf("\n");
 
