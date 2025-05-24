377ab325552f183b6070cbe1a5600852ed0fd087
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 16:02:50 +0100

    style

--- array_intersection.c
+++ array_intersection.c
@@ -4,11 +4,13 @@ int main() {
     int n1, n2;
     int arr1[100], arr2[100];
 
+    // Lire la taille du premier tableau
     scanf("%d", &n1);
     for (int i = 0; i < n1; i++) {
         scanf("%d", &arr1[i]);
     }
 
+    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     for (int i = 0; i < n2; i++) {
         scanf("%d", &arr2[i]);
@@ -16,21 +18,34 @@ int main() {
 
     printf("Intersection :");
 
-    for (int i = 0; i < n1; i++) {
-        int already_printed = 0;
+    int first_printed = 1;  // Variable pour gérer l'espace avant les nombres
 
+    // Parcourir arr1
+    for (int i = 0; i < n1; i++) {
+        int is_duplicate = 0;
+
+        // Vérifier si arr1[i] a déjà été imprimé pour éviter doublons
         for (int k = 0; k < i; k++) {
             if (arr1[i] == arr1[k]) {
-                already_printed = 1;
+                is_duplicate = 1;
                 break;
             }
         }
 
-        if (already_printed) continue;
+        if (is_duplicate)
+            continue;  // Passer à l'élément suivant
 
+        // Chercher arr1[i] dans arr2
         for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
-                printf(" %d", arr1[i]);
+                if (first_printed) {
+                    // Premier élément : pas d'espace avant
+                    printf(" %d", arr1[i]); 
+                    first_printed = 0;
+                } else {
+                    // Autres éléments : espace avant
+                    printf(" %d", arr1[i]);
+                }
                 break;
             }
         }
