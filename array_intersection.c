52e7c18d85caa61e6fde8ccd99f77dfc94a29490
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 14:07:19 +0100

    peace

--- array_intersection.c
+++ array_intersection.c
@@ -1,19 +1,43 @@
-#include <stdio.h> // Inclusion de la bibliothèque standard C
+#include <stdio.h>
 
 int main() {
-    int arr1[4] = {1, 2, 3, 4};
-    int arr2[4] = {3, 4, 5, 6};
+    int n1, n2;
+    scanf("%d", &n1); // Lecture de la taille du premier tableau
+
+    int arr1[n1];
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]); // Lecture des éléments du 1er tableau
+    }
+
+    scanf("%d", &n2); // Lecture de la taille du 2e tableau
+
+    int arr2[n2];
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]); // Lecture des éléments du 2e tableau
+    }
 
     printf("Intersection : ");
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < n1; i++) {
+        int already_printed = 0;
+        // Vérifier si l'élément arr1[i] a déjà été imprimé
+        for (int k = 0; k < i; k++) {
+            if (arr1[i] == arr1[k]) {
+                already_printed = 1;
+                break;
+            }
+        }
+
+        if (already_printed)
+            continue; // on passe à l’élément suivant
+
+        for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
                 printf("%d ", arr1[i]);
-                break; // Pour éviter d'imprimer plusieurs fois un même élément
+                break;
             }
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
