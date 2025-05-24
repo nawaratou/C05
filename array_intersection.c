ea35c2e4f57f2614fa9bfd5345120d79d5b5b5c6
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 22:07:21 +0100

    date

--- array_intersection.c
+++ array_intersection.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-    int n1, n2;
-    int arr1[100], arr2[100];
+int main() {
 
-    scanf("%d", &n1);
+    int n1, n2;
+
+    scanf("%d", &n1); 
+    int arr1[n1];
     for (int i = 0; i < n1; i++) {
         scanf("%d", &arr1[i]);
     }
 
     scanf("%d", &n2);
+    int arr2[n2];
     for (int i = 0; i < n2; i++) {
         scanf("%d", &arr2[i]);
     }
