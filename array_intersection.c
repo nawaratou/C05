145ca99b31c519694539dc3a2457388b9670b928
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 21:32:16 +0100

    crue

--- array_intersection.c
+++ array_intersection.c
@@ -2,35 +2,37 @@
 
 int main() {
     int n1, n2;
-    int arr1[100], arr2[100];
+    scanf("%d", &n1); // Lecture de la taille du premier tableau
 
-    scanf("%d", &n1);
+    int arr1[n1];
     for (int i = 0; i < n1; i++) {
-        scanf("%d", &arr1[i]);
+        scanf("%d", &arr1[i]); // Lecture des éléments du 1er tableau
     }
 
-    scanf("%d", &n2);
+    scanf("%d", &n2); // Lecture de la taille du 2e tableau
+
+    int arr2[n2];
     for (int i = 0; i < n2; i++) {
-        scanf("%d", &arr2[i]);
+        scanf("%d", &arr2[i]); // Lecture des éléments du 2e tableau
     }
 
-    printf("Intersection :");
-
+    printf("Intersection : ");
     for (int i = 0; i < n1; i++) {
-        // Vérifier doublon dans arr1 avant i
-        int doublon = 0;
+        int already_printed = 0;
+        // Vérifier si l'élément arr1[i] a déjà été imprimé
         for (int k = 0; k < i; k++) {
             if (arr1[i] == arr1[k]) {
-                doublon = 1;
+                already_printed = 1;
                 break;
             }
         }
-        if (doublon) continue;
 
-        // Vérifier si arr1[i] est dans arr2
+        if (already_printed)
+            continue; // on passe à l’élément suivant
+
         for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
-                printf(" %d", arr1[i]);
+                printf("%d ", arr1[i]);
                 break;
             }
         }
