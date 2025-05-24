bfec19c51dd33a0e41f06aa5d505868a011a07cb
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 23:52:22 +0100

    fer

--- array_intersection.c
+++ array_intersection.c
@@ -1,78 +1,51 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int main(void) {
-    int n1, n2;
+int main() {
+    int taille1, taille2;
     
-    // Lecture de la taille du premier tableau
-    if (scanf("%d", &n1) != 1 || n1 <= 0) {
-        printf("Erreur: taille invalide pour le premier tableau\n");
-        return 1;
-    }
-    
-    int arr1[n1];
-    for (int i = 0; i < n1; i++) {
-        if (scanf("%d", &arr1[i]) != 1) {
-            printf("Erreur: élément invalide dans le premier tableau\n");
-            return 1;
-        }
-    }
-
-    // Lecture de la taille du deuxième tableau
-    if (scanf("%d", &n2) != 1 || n2 <= 0) {
-        printf("Erreur: taille invalide pour le deuxième tableau\n");
-        return 1;
+    // Lecture des tableaux
+    scanf("%d", &taille1);
+    int tab1[taille1];
+    for(int i = 0; i < taille1; i++) {
+        scanf("%d", &tab1[i]);
     }
     
-    int arr2[n2];
-    for (int i = 0; i < n2; i++) {
-        if (scanf("%d", &arr2[i]) != 1) {
-            printf("Erreur: élément invalide dans le deuxième tableau\n");
-            return 1;
-        }
+    scanf("%d", &taille2);
+    int tab2[taille2];
+    for(int i = 0; i < taille2; i++) {
+        scanf("%d", &tab2[i]);
     }
-
+    
+    // Recherche de l'intersection
     printf("Intersection :");
-    bool found = false;
-
-    // Recherche des éléments communs
-    for (int i = 0; i < n1; i++) {
-        // Vérification des doublons dans arr1
-        bool duplicate = false;
-        for (int k = 0; k < i; k++) {
-            if (arr1[i] == arr1[k]) {
-                duplicate = true;
+    int intersection_trouvee = 0;
+    
+    for(int i = 0; i < taille1; i++) {
+        // Vérifier si c'est un doublon dans tab1
+        int doublon = 0;
+        for(int j = 0; j < i; j++) {
+            if(tab1[j] == tab1[i]) {
+                doublon = 1;
                 break;
             }
         }
-
-        if (duplicate) continue;
-
-        // Recherche dans le deuxième tableau
-        for (int j = 0; j < n2; j++) {
-            if (arr1[i] == arr2[j]) {
-                // Vérification des doublons dans arr2
-                bool duplicate_in_arr2 = false;
-                for (int l = 0; l < j; l++) {
-                    if (arr2[l] == arr2[j]) {
-                        duplicate_in_arr2 = true;
-                        break;
-                    }
-                }
-                
-                if (!duplicate_in_arr2) {
-                    printf(" %d", arr1[i]);
-                    found = true;
+        
+        if(!doublon) {
+            // Chercher dans tab2
+            for(int k = 0; k < taille2; k++) {
+                if(tab1[i] == tab2[k]) {
+                    printf(" %d", tab1[i]);
+                    intersection_trouvee = 1;
                     break;
                 }
             }
         }
     }
-
-    if (!found) {
+    
+    if(!intersection_trouvee) {
         printf(" Aucun");
     }
     printf("\n");
-
+    
     return 0;
 }
