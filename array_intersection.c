597a13c8e8e9cd42522df998e2e01902b81a223b
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 22:27:48 +0100

    pir

--- array_intersection.c
+++ array_intersection.c
@@ -2,41 +2,50 @@
 
 int main(void) {
     int n1, n2;
-    int arr1[100], arr2[100]; //  tailles fixes autorisées
+    int arr1[100], arr2[100], intersection[100];
+    int inter_size = 0;
 
+    // Lecture du premier tableau
     scanf("%d", &n1);
     for (int i = 0; i < n1; i++) {
         scanf("%d", &arr1[i]);
     }
 
+    // Lecture du deuxième tableau
     scanf("%d", &n2);
     for (int i = 0; i < n2; i++) {
         scanf("%d", &arr2[i]);
     }
 
-    printf("Intersection :");
-
+    // Recherche de l'intersection
     for (int i = 0; i < n1; i++) {
-        int already_seen = 0;
-
-        // éviter les doublons
-        for (int k = 0; k < i; k++) {
-            if (arr1[i] == arr1[k]) {
-                already_seen = 1;
-                break;
-            }
-        }
-
-        if (already_seen) continue;
-
+        // Vérifier si arr1[i] est présent dans arr2
         for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
-                printf(" %d", arr1[i]);
-                break;
+                // Vérifier si arr1[i] est déjà dans intersection[]
+                int already_in_intersection = 0;
+                for (int k = 0; k < inter_size; k++) {
+                    if (intersection[k] == arr1[i]) {
+                        already_in_intersection = 1;
+                        break;
+                    }
+                }
+                // Si ce n'est pas un doublon, l'ajouter à intersection[]
+                if (!already_in_intersection) {
+                    intersection[inter_size] = arr1[i];
+                    inter_size++;
+                }
+                break; // Passer à l'élément suivant de arr1
             }
         }
     }
 
+    // Affichage de l'intersection
+    printf("Intersection :");
+    for (int i = 0; i < inter_size; i++) {
+        printf(" %d", intersection[i]);
+    }
     printf("\n");
+
     return 0;
 }
