833e7b8e54eb261365ba3cc43237f4df8b06d172
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 21:58:27 +0100

    apple

--- array_intersection.c
+++ array_intersection.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int n1, n2;
-    scanf("%d", &n1); // Lecture de la taille du premier tableau
+    int arr1[100], arr2[100];
 
-    int arr1[n1];
+    scanf("%d", &n1);
     for (int i = 0; i < n1; i++) {
-        scanf("%d", &arr1[i]); // Lecture des éléments du 1er tableau
+        scanf("%d", &arr1[i]);
     }
 
-    scanf("%d", &n2); // Lecture de la taille du 2e tableau
-
-    int arr2[n2];
+    scanf("%d", &n2);
     for (int i = 0; i < n2; i++) {
-        scanf("%d", &arr2[i]); // Lecture des éléments du 2e tableau
+        scanf("%d", &arr2[i]);
     }
 
-    printf("Intersection : ");
+    printf("Intersection :");
+
     for (int i = 0; i < n1; i++) {
-        int already_printed = 0;
-        // Vérifier si l'élément arr1[i] a déjà été imprimé
+        int already_seen = 0;
         for (int k = 0; k < i; k++) {
             if (arr1[i] == arr1[k]) {
-                already_printed = 1;
+                already_seen = 1;
                 break;
             }
         }
 
-        if (already_printed)
-            continue; // on passe à l’élément suivant
+        if (already_seen)
+            continue;
 
         for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
-                printf("%d ", arr1[i]);
+                printf(" %d", arr1[i]);
                 break;
             }
         }
