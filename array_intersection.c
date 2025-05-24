4cf49de6da3cd468c0f22625f0ed69e884a87777
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-24 15:45:16 +0100

    azerty

--- array_intersection.c
+++ array_intersection.c
@@ -2,37 +2,40 @@
 
 int main() {
     int n1, n2;
-    long arr1[100], arr2[100]; // Taille max fixée à 100 pour éviter les erreurs
+    int arr1[100], arr2[100];
 
+    // Lire la taille du 1er tableau
     scanf("%d", &n1);
     for (int i = 0; i < n1; i++) {
-        scanf("%ld", &arr1[i]);
+        scanf("%d", &arr1[i]);
     }
 
+    // Lire la taille du 2e tableau
     scanf("%d", &n2);
     for (int i = 0; i < n2; i++) {
-        scanf("%ld", &arr2[i]);
+        scanf("%d", &arr2[i]);
     }
 
-    printf("Intersection : ");
-    for (int i = 0; i < n1; i++) {
-        int already_printed = 0;
+    // Affichage de l'en-tête
+    printf("Intersection :");
 
-        // Vérifie si arr1[i] a déjà été affiché
+    for (int i = 0; i < n1; i++) {
+        int is_duplicate = 0;
+
+        // Vérifier si arr1[i] a déjà été considéré (pour éviter les doublons)
         for (int k = 0; k < i; k++) {
             if (arr1[i] == arr1[k]) {
-                already_printed = 1;
+                is_duplicate = 1;
                 break;
             }
         }
 
-        if (already_printed)
-            continue;
+        if (is_duplicate) continue;
 
-        // Vérifie si arr1[i] est présent dans arr2
+        // Vérifier si arr1[i] est présent dans arr2
         for (int j = 0; j < n2; j++) {
             if (arr1[i] == arr2[j]) {
-                printf("%ld ", arr1[i]);
+                printf(" %d", arr1[i]); // Affichage avec espace *avant* le chiffre
                 break;
             }
         }
