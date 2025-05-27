ab93fdf77bf7b818b2f8ac784dff954d7e64c3bf
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-27 11:45:44 +0100

    chance

--- array_intersection.c
+++ array_intersection.c
@@ -1,53 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// Fonction pour vérifier si un élément est déjà présent dans le tableau résultat
+bool estDejaPresent(int tab[], int taille, int element) {
+    for (int i = 0; i < taille; i++) {
+        if (tab[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
-    int size1, size2;
-    
-    // Lecture du premier tableau
-    scanf("%d", &size1);
-    int arr1[size1];
-    for (int i = 0; i < size1; i++) {
-        scanf("%d", &arr1[i]);
+    int n1, n2;
+
+    // Lecture de la taille du premier tableau
+    scanf("%d", &n1);
+    int tab1[n1];
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", &tab1[i]);
     }
 
-    // Lecture du deuxième tableau
-    scanf("%d", &size2);
-    int arr2[size2];
-    for (int i = 0; i < size2; i++) {
-        scanf("%d", &arr2[i]);
+    // Lecture de la taille du second tableau
+    scanf("%d", &n2);
+    int tab2[n2];
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &tab2[i]);
     }
 
+    int intersection[n1 < n2 ? n1 : n2]; // Taille max possible de l'intersection
+    int k = 0; // Compteur pour l'intersection
+
+    // Parcours croisé pour trouver les éléments communs
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (tab1[i] == tab2[j]) {
+                // Vérifier que ce n'est pas déjà dans le tableau résultat
+                if (!estDejaPresent(intersection, k, tab1[i])) {
+                    intersection[k++] = tab1[i];
+                }
+            }
+        }
+    }
+
+    // Affichage du résultat
     printf("Intersection :");
-    int found = 0;
-
-    for (int i = 0; i < size1; i++) {
-        int value = arr1[i];
-
-        // Vérifie si value est déjà affichée
-        int already_printed = 0;
-        for (int j = 0; j < i; j++) {
-            if (arr1[j] == value) {
-                already_printed = 1;
-                break;
-            }
-        }
-
-        if (already_printed) continue;
-
-        // Cherche si value existe dans arr2
-        for (int k = 0; k < size2; k++) {
-            if (arr2[k] == value) {
-                printf(" %d", value);
-                found = 1;
-                break;
-            }
-        }
+    for (int i = 0; i < k; i++) {
+        printf(" %d", intersection[i]);
     }
-
-    if (!found) {
-        printf(" Aucun");
-    }
-
     printf("\n");
+
     return 0;
 }
