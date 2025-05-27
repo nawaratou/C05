0b32db9ceb3758426666fdaa2e1fe8474adf23d9
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-27 12:11:42 +0100

    brave

--- array_intersection.c
+++ array_intersection.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2, i, j, k;
-    int tab1[100], tab2[100], inter[100];
-    int interSize = 0;
+    int n1, n2;
+    int tab1[100], tab2[100];
+    int i, j, k;
+    int inter[100]; // Pour stocker l'intersection
+    int interCount = 0;
     int deja;
 
-    // Lire la taille du premier tableau
+    // Lire la taille et les éléments du premier tableau
     scanf("%d", &n1);
     for (i = 0; i < n1; i++) {
         scanf("%d", &tab1[i]);
     }
 
-    // Lire la taille du deuxième tableau
+    // Lire la taille et les éléments du deuxième tableau
     scanf("%d", &n2);
     for (i = 0; i < n2; i++) {
         scanf("%d", &tab2[i]);
     }
 
-    // Rechercher les éléments communs
+    // Trouver l'intersection
     for (i = 0; i < n1; i++) {
         for (j = 0; j < n2; j++) {
             if (tab1[i] == tab2[j]) {
-                // Vérifier si l'élément est déjà dans inter[]
+                // Vérifier si déjà ajouté à inter[]
                 deja = 0;
-                for (k = 0; k < interSize; k++) {
+                for (k = 0; k < interCount; k++) {
                     if (inter[k] == tab1[i]) {
                         deja = 1;
                         break;
                     }
                 }
                 if (!deja) {
-                    inter[interSize] = tab1[i];
-                    interSize++;
+                    inter[interCount] = tab1[i];
+                    interCount++;
                 }
                 break;
             }
         }
     }
 
-    // Affichage
+    // Affichage conforme à l'exemple
     printf("Intersection :");
-    for (i = 0; i < interSize; i++) {
+    for (i = 0; i < interCount; i++) {
         printf(" %d", inter[i]);
     }
     printf("\n");
