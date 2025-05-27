379e09c6cae8c1ce8974e10cf02525504ae9d9e1
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-27 12:20:12 +0100

    don

--- array_intersection.c
+++ array_intersection.c
@@ -2,38 +2,40 @@
 
 int main() {
     int n1, n2;
-    int tab1[100], tab2[100], i, j;
+    int tab1[100], tab2[100];
+    int i, j, k;
+    int deja;
 
-    // Lire la taille et les éléments du premier tableau
+    // Lire la taille du premier tableau
     scanf("%d", &n1);
     for (i = 0; i < n1; i++) {
         scanf("%d", &tab1[i]);
     }
 
-    // Lire la taille et les éléments du deuxième tableau
+    // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     for (i = 0; i < n2; i++) {
         scanf("%d", &tab2[i]);
     }
 
-    // Affichage de l'intersection
     printf("Intersection :");
+
+    // Vérifier les éléments communs
     for (i = 0; i < n1; i++) {
         for (j = 0; j < n2; j++) {
             if (tab1[i] == tab2[j]) {
-                // Vérifie si c’est déjà affiché
-                int deja_affiche = 0;
-                int k;
+                // Vérifie si déjà affiché
+                deja = 0;
                 for (k = 0; k < i; k++) {
                     if (tab1[k] == tab1[i]) {
-                        deja_affiche = 1;
+                        deja = 1;
                         break;
                     }
                 }
-                if (!deja_affiche) {
+                if (!deja) {
                     printf(" %d", tab1[i]);
                 }
-                break; // On passe à l’élément suivant de tab1
+                break;
             }
         }
     }
