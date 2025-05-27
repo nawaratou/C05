29dca8e185889667d5fb5f4eadc71210c930c2c2
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-27 12:06:17 +0100

    bien

--- array_intersection.c
+++ array_intersection.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2;
-    
+    int n1, n2, i, j, k;
+    int tab1[100], tab2[100], inter[100];
+    int interSize = 0;
+    int deja;
+
     // Lire la taille du premier tableau
     scanf("%d", &n1);
-    int tab1[n1];
-    for (int i = 0; i < n1; i++) {
+    for (i = 0; i < n1; i++) {
         scanf("%d", &tab1[i]);
     }
 
     // Lire la taille du deuxième tableau
     scanf("%d", &n2);
-    int tab2[n2];
-    for (int i = 0; i < n2; i++) {
+    for (i = 0; i < n2; i++) {
         scanf("%d", &tab2[i]);
     }
 
-    // Afficher les éléments communs (intersection)
-    printf("Intersection :");
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
+    // Rechercher les éléments communs
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
             if (tab1[i] == tab2[j]) {
-                // Vérifie si cet élément a déjà été affiché
-                int deja_affiche = 0;
-                for (int k = 0; k < i; k++) {
-                    if (tab1[k] == tab1[i]) {
-                        deja_affiche = 1;
+                // Vérifier si l'élément est déjà dans inter[]
+                deja = 0;
+                for (k = 0; k < interSize; k++) {
+                    if (inter[k] == tab1[i]) {
+                        deja = 1;
                         break;
                     }
                 }
-                if (!deja_affiche) {
-                    printf(" %d", tab1[i]);
+                if (!deja) {
+                    inter[interSize] = tab1[i];
+                    interSize++;
                 }
-                break; // Une fois trouvé, pas besoin de continuer
+                break;
             }
         }
+    }
+
+    // Affichage
+    printf("Intersection :");
+    for (i = 0; i < interSize; i++) {
+        printf(" %d", inter[i]);
     }
     printf("\n");
 
