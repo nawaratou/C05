dff5025c6cda016d1f4ee24ac961b9590d4bc355
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-29 18:20:46 +0100

    ret

--- array_intersection.c
+++ array_intersection.c
@@ -1,52 +1,40 @@
 #include <stdio.h>
 
-#define MAX_SIZE 100
+// Function to find intersection of two arrays
+void intersection(int a[], int n1, int b[], int n2, int res[], int *res_size) {
+  
+    // Traverse through a[] and search every element a[i] in b[]
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
 
-// Vérifie si une valeur est déjà présente dans un tableau
-int existe_deja(int tab[], int taille, int valeur) {
-    int i;
-    for (i = 0; i < taille; i++) {
-        if (tab[i] == valeur) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int main() {
-    int n1, n2;
-    int tab1[MAX_SIZE], tab2[MAX_SIZE];
-    int inter[MAX_SIZE];
-    int i, j, k = 0;
-
-    scanf("%d", &n1);
-    for (i = 0; i < n1; i++) {
-        scanf("%d", &tab1[i]);
-    }
-
-    scanf("%d", &n2);
-    for (i = 0; i < n2; i++) {
-        scanf("%d", &tab2[i]);
-    }
-
-    // Comparaison croisée pour trouver l'intersection sans doublons
-    for (i = 0; i < n1; i++) {
-        for (j = 0; j < n2; j++) {
-            if (tab1[i] == tab2[j]) {
-                if (!existe_deja(inter, k, tab1[i])) {
-                    inter[k++] = tab1[i];
+            // If found, check if the element is already in the result
+            int found = 0;
+            if (a[i] == b[j]) {
+                for (int k = 0; k < *res_size; k++) {
+                    if (res[k] == a[i]) {
+                        found = 1;
+                        break;
+                    }
                 }
-                break;
+                if (!found) {
+                    res[(*res_size)++] = a[i];
+                }
             }
         }
     }
+}
 
-    // Affichage du résultat sans espace final
-    printf("Intersection :");
-    for (i = 0; i < k; i++) {
-        printf(" %d", inter[i]);
+int main() {
+    int a[] = {1, 2, 3, 2, 1};
+    int b[] = {3, 2, 2, 3, 3, 2};
+    int res[10];
+    int res_size = 0;
+
+    intersection(a, 5, b, 6, res, &res_size);
+
+    for (int i = 0; i < res_size; i++) {
+        printf("%d ", res[i]);
     }
-    printf("\n");
 
     return 0;
 }
