479746e7881ca14a051a3b021ed4159761d6fd05
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-29 23:16:46 +0100

    styl

--- array_intersection.c
+++ array_intersection.c
@@ -3,7 +3,6 @@
 int main() {
     int n;
     scanf("%d", &n);
-
     int a[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
@@ -11,28 +10,20 @@ int main() {
 
     int m;
     scanf("%d", &m);
-
     int b[m];
     for (int i = 0; i < m; i++) {
         scanf("%d", &b[i]);
     }
 
-    int res[100];
-    int k = 0; 
-    
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (a[i] == b[j]) {
-                        res[k++] = a[i];
-                    }
-                }
+    printf("Intersection :");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i] == b[j]) {
+                printf(" %d", a[i]);
             }
-        
-            printf("Intersection :");
-            for (int i = 0; i < k; i++) {
-                printf(" %d", res[i]);
-            }
-            printf("\n");
-        
-            return 0;
         }
+    }
+
+    printf("\n");
+    return 0;
+}
