19f6e4a88343dce55f44bc3185aae89ef8bb4588
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-29 22:09:54 +0100

    sert

--- array_intersection.c
+++ array_intersection.c
@@ -1,41 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    int m;
-    int a[100];
-    int b[100];
-    int res[100];
-    int i;
-    int j;
-    int k;
-    int x;
-    int existe;
-
-    k = 0;
-
-    // Lire la taille du premier tableau
+    int n; // Taille du premier tableau
     scanf("%d", &n);
 
-    // Lire les éléments du premier tableau
-    for (i = 0; i < n; i++) {
+    int a[100]; // Premier tableau
+    for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    // Lire la taille du deuxième tableau
+    int m; // Taille du deuxième tableau
     scanf("%d", &m);
 
-    // Lire les éléments du deuxième tableau
-    for (i = 0; i < m; i++) {
+    int b[100]; // Deuxième tableau
+    for (int i = 0; i < m; i++) {
         scanf("%d", &b[i]);
     }
 
-    // Comparaison des deux tableaux
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+    int res[100]; // Tableau pour stocker l’intersection
+    int k = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             if (a[i] == b[j]) {
-                existe = 0;
-                for (x = 0; x < k; x++) {
+                int existe = 0;
+                for (int x = 0; x < k; x++) {
                     if (res[x] == a[i]) {
                         existe = 1;
                         break;
@@ -50,9 +39,8 @@ int main() {
         }
     }
 
-    // Affichage
     printf("Intersection :");
-    for (i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
         printf(" %d", res[i]);
     }
     printf("\n");
