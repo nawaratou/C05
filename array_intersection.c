a5bd099d338d359cf6a091f6bc92bc158ee46808
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-30 00:52:26 +0100

    type

--- array_intersection.c
+++ array_intersection.c
@@ -1,55 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n); // taille premier tableau
+    int n, m;
+
+    // Lecture du tableau A
+    scanf("%d", &n);
     int a[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    int m;
-    scanf("%d", &m); // taille deuxième tableau
+    // Lecture du tableau B
+    scanf("%d", &m);
     int b[m];
     for (int i = 0; i < m; i++) {
         scanf("%d", &b[i]);
     }
 
-    // On va afficher tous les éléments dans une variable temporaire,
-    // puis on affiche le label et ensuite les éléments.
-
-    // Pour rester simple, on affiche directement dans la boucle,
-    // mais on garde le label après.
-
-    // Solution simple : on garde les éléments trouvés dans res[].
-
-    int res[n + m]; // grand tableau temporaire, suffisant pour tout stocker
-    int k = 0;
-
+    printf("Intersection :");
     for (int i = 0; i < n; i++) {
+        // Vérifier si a[i] a déjà été traité (éviter les doublons)
+        int deja_traite = 0;
+        for (int k = 0; k < i; k++) {
+            if (a[k] == a[i]) {
+                deja_traite = 1;
+                break;
+            }
+        }
+        if (deja_traite) continue;
+
+        // Vérifier si a[i] est dans B
         for (int j = 0; j < m; j++) {
             if (a[i] == b[j]) {
-                // On évite les doublons dans res
-                int exist = 0;
-                for (int x = 0; x < k; x++) {
-                    if (res[x] == a[i]) {
-                        exist = 1;
-                        break;
-                    }
-                }
-                if (!exist) {
-                    res[k++] = a[i];
-                }
+                printf(" %d", a[i]);
                 break;
             }
         }
     }
-
-    printf("Intersection :");
-    for (int i = 0; i < k; i++) {
-        printf(" %d", res[i]);
-    }
     printf("\n");
-
     return 0;
 }
