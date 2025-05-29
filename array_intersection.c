2c4d12fd35426d2f92d94c456b19fcf82346f688
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-29 22:27:51 +0100

    arbre

--- array_intersection.c
+++ array_intersection.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n; // Taille du premier tableau
+    int n;
     scanf("%d", &n);
 
-    int a[100]; // Premier tableau
+    int *a = malloc(n * sizeof(int)); // allocation dynamique pour le 1er tableau
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    int m; // Taille du deuxième tableau
+    int m;
     scanf("%d", &m);
 
-    int b[100]; // Deuxième tableau
+    int *b = malloc(m * sizeof(int)); // allocation dynamique pour le 2ème tableau
     for (int i = 0; i < m; i++) {
         scanf("%d", &b[i]);
     }
 
-    int res[100]; // Tableau pour stocker l’intersection
+    int *res = malloc((n < m ? n : m) * sizeof(int)); // max taille de l'intersection
     int k = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (a[i] == b[j]) {
-                int existe = 0;
+                int exist = 0;
                 for (int x = 0; x < k; x++) {
                     if (res[x] == a[i]) {
-                        existe = 1;
+                        exist = 1;
                         break;
                     }
                 }
-                if (existe == 0) {
+                if (!exist) {
                     res[k] = a[i];
                     k++;
                 }
@@ -44,6 +45,11 @@ int main() {
         printf(" %d", res[i]);
     }
     printf("\n");
+
+    // Libération de la mémoire
+    free(a);
+    free(b);
+    free(res);
 
     return 0;
 }
