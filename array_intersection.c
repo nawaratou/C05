5dcc5f9a2ed5ed22ce5f0c73a5a7e7831b596db4
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-30 12:21:19 +0100

    true

--- array_intersection.c
+++ array_intersection.c
@@ -4,27 +4,27 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int tab1[n];
+    int a[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &tab1[i]);
+        scanf("%d", &a[i]);
     }
 
     int m;
     scanf("%d", &m);
 
-    int tab2[m];
+    int b[m];
     for (int i = 0; i < m; i++) {
-        scanf("%d", &tab2[i]);
+        scanf("%d", &b[i]);
     }
 
     // Afficher les éléments présents dans les deux tableaux (intersection)
     printf("Intersection : ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0;i < n; i++) {
 
         // Étape 1 : vérifier si tab1[i] existe dans tab2
         int found = 0;
         for (int j = 0; j < m; j++) {
-            if (tab1[i] == tab2[j]) {
+            if (a[i] == b[j]) {
                 found = 1;
                 break; // inutile de continuer
             }
@@ -33,7 +33,7 @@ int main() {
         // Étape 2 : éviter les doublons (si déjà apparu dans tab1)
         int deja_affiche = 0;
         for (int k = 0; k < i; k++) {
-            if (tab1[i] == tab1[k]) {
+            if (a[i] == a[k]) {
                 deja_affiche = 1;
                 break;
             }
@@ -41,7 +41,7 @@ int main() {
 
         // Étape 3 : afficher seulement si trouvé dans tab2 et pas encore affiché
         if (found && !deja_affiche) {
-            printf("%d ", tab1[i]);
+            printf("%d ", a[i]);
         }
     }
 
