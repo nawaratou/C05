26ff85286ddde4421fe63d1af97e09c5420abf38
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-30 11:13:16 +0100

    port

--- array_intersection.c
+++ array_intersection.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    int n;
+int main(void)
+{
+    int n, m;
+    int i, j, k;
+    int tab1[100], tab2[100];
+
+    // Lecture de la taille du premier tableau
     scanf("%d", &n);
-    int tab1[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &tab1[i]);
-    }
+    for (i = 0; i < n; i++)
+        scanf("%d", &tab1[i]); // Lecture un à un
 
-    int m;
+    // Lecture de la taille du deuxième tableau
     scanf("%d", &m);
-    int tab2[m];
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &tab2[i]);
-    }
+    for (i = 0; i < m; i++)
+        scanf("%d", &tab2[i]); // Lecture un à un
 
+    // Affichage de l'intersection sans doublons
     printf("Intersection :");
-    for (int i = 0; i < n; i++) {
-        // Vérifier si tab1[i] existe dans tab2
-        for (int j = 0; j < m; j++) {
-            if (tab1[i] == tab2[j]) {
-                // Vérifier si on a déjà affiché ce nombre
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (tab1[i] == tab2[j])
+            {
+                // Vérifier si déjà affiché
                 int deja_affiche = 0;
-                for (int k = 0; k < i; k++) {
-                    if (tab1[k] == tab1[i]) {
+                for (k = 0; k < i; k++)
+                {
+                    if (tab1[k] == tab1[i])
+                    {
                         deja_affiche = 1;
                         break;
                     }
                 }
-                if (!deja_affiche) {
+                if (!deja_affiche)
                     printf(" %d", tab1[i]);
-                }
                 break;
             }
         }
     }
     printf("\n");
+
     return 0;
 }
