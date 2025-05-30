a5f80d6b48e76a04a4833e098ba8c3c5869e1396
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-30 02:14:07 +0100

    rime

--- array_intersection.c
+++ array_intersection.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n;  // taille du premier tableau
     scanf("%d", &n);
-    int a[n];
+
+    int a[n];  // premier tableau
     for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &a[i]);  // lecture des éléments de tab1
     }
 
-    int m;
+    int m;  // taille du deuxième tableau
     scanf("%d", &m);
-    int b[m];
+
+    int b[m];  // deuxième tableau
     for (int i = 0; i < m; i++) {
-        scanf("%d", &b[i]);
+        scanf("%d", &b[i]);  // lecture des éléments de tab2
     }
 
+    // Affichage de l'intersection
     printf("Intersection :");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (a[i] == b[j]) {
-                printf(" %d", a[i]);
-                break;
+                printf(" %d", a[i]);  // élément commun trouvé
+                break;                   // on ne veut pas répéter ce même élément
             }
         }
     }
-
     printf("\n");
+
     return 0;
 }
